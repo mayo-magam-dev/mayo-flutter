5438f4279d4c4d48797e685de8737f9e096ae5a6
Jihyuk Lee <lead@sver.dev>
2025-01-04 17:03:21 +0900

    style :: 로그인 페이지 퍼블리싱 준비

---
+++ lib/view/login/login_scaffold.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class _Scaffold extends StatelessWidget {
+  const _Scaffold(
+      {required this.logo, required this.form, required this.socialLogin});
+  final Widget logo;
+  final Widget form;
+  final Widget socialLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 70.w, horizontal: 24.h),
+        child: Column(
+          children: [
+            logo,
+            SizedBox(height: 70.h),
+            form,
+            SizedBox(height: 54.h),
+            socialLogin,
+          ],
+        ),
+      ),
+    ));
+  }
+}
