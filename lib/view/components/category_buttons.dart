3e022ff29cdd70de64ed5eac6e7e4c61f5264748
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-01-16 23:04:56 +0900

    style :: 중단 부분 위젯분리

---
+++ lib/view/components/category_buttons.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CategoryButtons extends StatelessWidget {
+  const CategoryButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: [
+            SvgPicture.asset('assets/icons/category_logo.svg'),
+            SizedBox(height: 8.h),
+            Text(
+              '전체',
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.28,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            SvgPicture.asset('assets/icons/category_cooking.svg'),
+            SizedBox(height: 8.h),
+            Text(
+              '든든한 식사',
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.28,
+              ),
+            )
+          ],
+        ),
+        Column(
+          children: [
+            SvgPicture.asset('assets/icons/category_cake.svg'),
+            SizedBox(height: 8.h),
+            Text(
+              '가벼운 간식',
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.28,
+              ),
+            )
+          ],
+        ),
+        Column(
+          children: [
+            SvgPicture.asset('assets/icons/category_dish.svg'),
+            SizedBox(height: 8.h),
+            Text(
+              '제휴업체',
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.28,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
