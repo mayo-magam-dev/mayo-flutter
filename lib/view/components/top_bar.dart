fc1f7b440edc4fa8566cf71596459b024c8093eb
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-01-25 23:15:30 +0900

    style :: 로고 변경

--- lib/view/components/top_bar.dart
+++ lib/view/components/top_bar.dart
@@ -10,23 +10,24 @@ class Topbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SvgPicture.asset("assets/icons/arrow.svg"),
-            Text(
-              title,
-              style: AppTextStyle.body1Medium,
-            ),
-            showCarts
-                ? SvgPicture.asset("assets/icons/cart.svg")
-                : SizedBox(
-                    height: 30,
-                    width: 30,
-                  )
-          ],
-        ));
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/icons/arrow.svg'),
+          Text(
+            title,
+            style: AppTextStyle.body1Medium,
+          ),
+          showCarts
+              ? SvgPicture.asset("assets/icons/cart.svg")
+              : SizedBox(
+                  height: 30,
+                  width: 30,
+                ),
+        ],
+      ),
+    );
   }
 }
