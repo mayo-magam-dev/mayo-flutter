63627c9743b729c37aea7f8a9c1b5ad22e384a63
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-01-31 19:35:22 +0900

    style:: step2 퍼블리싱 완성

---
+++ lib/view/signUp/step2/sign_up_step2_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mayo_flutter/desginSystem/color.dart';
+import 'package:mayo_flutter/view/components/top_bar.dart';
+import 'package:mayo_flutter/desginSystem/fontsize.dart';
+
+part 'sign_up_scaffold.dart';
+part 'sign_up_header.dart';
+part 'sign_up_middle.dart';
+
+class SignUpStep2Page extends StatelessWidget {
+  const SignUpStep2Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _Scaffold(
+      topBar: Topbar(title: '회원가입', showCarts: false),
+      header: _SignUpHeader(),
+      middle: _SignUpMiddle(),
+      
+    );
+  }
+}
