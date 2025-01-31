d2607f3e9f9cbd99e0bbcb9c8b6514a34d222829
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-01-31 19:34:47 +0900

    chore ::  파일명 수정

---
+++ lib/view/signUp/step1/sign_up_scaffold.dart
@@ -0,0 +1,41 @@
+part of 'sign_up_step1_page.dart';
+
+class _Scaffold extends StatelessWidget {
+  const _Scaffold({
+    required this.topBar,
+    required this.header,
+    required this.middle,
+  });
+  final Widget topBar;
+  final Widget header;
+  final Widget middle;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            topBar,
+            Container(
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    width: 1,
+                    color: GlobalMainGrey.grey200,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 90.h),
+            header,
+            SizedBox(height: 82.h),
+            middle,
+          ],
+        ),
+      ),
+    );
+  }
+}
