42c03402cd2d08607e13b5727a729d617abc5953
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-02-03 21:13:46 +0900

    회원 탈퇴 페이지 퍼블리싱 완성

---
+++ lib/view/my/detail_pages/acount_delete_page/account_delete_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mayo_flutter/desginSystem/color.dart';
+import 'package:mayo_flutter/desginSystem/fontsize.dart';
+import 'package:mayo_flutter/view/components/top_bar.dart';
+import 'package:mayo_flutter/view/my/my_page.dart';
+
+part 'account_delete_scaffold.dart';
+part 'account_delete_main.dart';
+
+class AccountDeletePage extends StatelessWidget {
+  const AccountDeletePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _Scaffold(
+      topBar: Topbar(title: '회원 탈퇴', showCarts: false),
+      main: _Main(),
+    );
+  }
+}
