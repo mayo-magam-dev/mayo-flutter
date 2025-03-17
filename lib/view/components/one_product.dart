de8dc78576c220e4724226cdde0c16f20229abd2
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-03-17 09:32:40 +0900

    Revert "chore :: 가게 리스트 순서 정렬"
    This reverts commit 137709d30dbfa3daf774b72065303e5facf657c1.

--- lib/view/components/one_product.dart
+++ lib/view/components/one_product.dart
@@ -1,134 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:mayo_flutter/dataSource/store.dart';
 import 'package:mayo_flutter/designSystem/color.dart';
 import 'package:mayo_flutter/designSystem/fontsize.dart';
-
 class OneProductWidget extends StatelessWidget {
-  OneProductWidget({
+  const OneProductWidget({
     super.key,
-    required this.storeIndex,
   });
-
-  int storeIndex;
-
-  var data = StoreDataSource().getAllStores();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: data,
-      builder: (context, snapshot) {
-        if (snapshot.hasData == false) {
-          return Center(child: CircularProgressIndicator());
-        }
-        return Column(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 21.h),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Image.asset(
+            "assets/images/empty_cart.png",
+            width: 342.w,
+            height: 123.h,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "힘쎈드위치",
+                  style: AppTextStyle.body1Bold,
+                ),
+                SizedBox(
+                  height: 2.h,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset("assets/icons/location.svg"),
+                    SizedBox(
+                      width: 3.w,
+                    ),
+                    Text("광주광역시 동구 필문대로273번길 4",
+                        style: AppTextStyle.body2Medium),
+                  ],
+                ),
+                SizedBox(
+                  height: 5.h,
+                ),
+                Row(children: [
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 21.h),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Image.asset(
-                      "assets/images/empty_cart.png",
-                      width: 342.w,
-                      height: 123.h,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            snapshot.data![storeIndex].storeName,
-                            style: AppTextStyle.body1Bold,
-                          ),
-                          SizedBox(
-                            height: 2.h,
-                          ),
-                          Row(
-                            children: [
-                              SvgPicture.asset("assets/icons/location.svg"),
-                              SizedBox(
-                                width: 3.w,
-                              ),
-                              Text(snapshot.data![storeIndex].address,
-                                  style: AppTextStyle.body2Medium),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 5.h,
-                          ),
-                          Row(children: [
-                            Container(
-                                padding:
-                                    EdgeInsets.symmetric(horizontal: 7.5.w),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: GlobalMainColor.globalButtonColor,
-                                ),
-                                child: Text("포장",
-                                    style: AppTextStyle.captionMedium)),
-                            SizedBox(
-                              width: 5.w,
-                            ),
-                            Container(
-                                padding:
-                                    EdgeInsets.symmetric(horizontal: 7.5.w),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(
-                                      color: GlobalMainColor.globalButtonColor),
-                                ),
-                                child: Text("매장",
-                                    style: AppTextStyle.captionMedium)),
-                          ]),
-                        ],
+                      padding: EdgeInsets.symmetric(horizontal: 7.5.w),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: GlobalMainColor.globalButtonColor,
                       ),
-                      Container(
-                          width: 67.w,
-                          height: 67.h,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: GlobalMainColor.globalButtonColor,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/icons/time.svg",
-                                colorFilter: ColorFilter.mode(
-                                    Colors.white, BlendMode.srcIn),
-                                width: 33.w,
-                                height: 33.h,
-                              ),
-                              Text(
-                                "예약하기",
-                                style: AppTextStyle.captionMedium
-                                    .copyWith(color: Colors.white),
-                              ),
-                            ],
-                          )),
-                    ],
-                  )
-                ],
-              ),
+                      child: Text("포장", style: AppTextStyle.captionMedium)),
+                  SizedBox(
+                    width: 5.w,
+                  ),
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 7.5.w),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                            color: GlobalMainColor.globalButtonColor),
+                      ),
+                      child: Text("매장", style: AppTextStyle.captionMedium)),
+                ]),
+              ],
             ),
-            Divider(
-              color: GlobalMainGrey.grey200,
-              thickness: 2,
-            ),
+            Container(
+                width: 67.w,
+                height: 67.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: GlobalMainColor.globalButtonColor,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      "assets/icons/time.svg",
+                      colorFilter:
+                          ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                      width: 33.w,
+                      height: 33.h,
+                    ),
+                    Text(
+                      "예약하기",
+                      style: AppTextStyle.captionMedium
+                          .copyWith(color: Colors.white),
+                    ),
+                  ],
+                )),
           ],
-        );
-      },
+        )
+      ],
     );
   }
 }
--- lib/view/sub/onsale_page.dart
+++ lib/view/sub/onsale_page.dart
@@ -11,83 +11,28 @@ class OnsalePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
+      body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Topbar(title: "전체", showCarts: true),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("오늘은 뭘 먹을까?", style: AppTextStyle.heading2Bold),
-                    Text("지금 업데이트된 우리 동네 마감 할인 정보",
-                        style: AppTextStyle.body1Medium
-                            .copyWith(color: GlobalMainGrey.grey700)),
-                  ],
-                ),
-              ),
-              //7
-              OneProductWidget(
-                storeIndex: 0,
-              ),
-              OneProductWidget(
-                storeIndex: 4,
-              ),
-              OneProductWidget(
-                storeIndex: 5,
-              ),
-              OneProductWidget(
-                storeIndex: 6,
-              ),
-              OneProductWidget(
-                storeIndex: 7,
-              ),
-              OneProductWidget(
-                storeIndex: 8,
-              ),
-              OneProductWidget(
-                storeIndex: 9,
-              ),
-              OneProductWidget(
-                storeIndex: 10,
-              ),
-              OneProductWidget(
-                storeIndex: 11,
-              ),
-              OneProductWidget(
-                storeIndex: 12,
-              ),
-              OneProductWidget(
-                storeIndex: 13,
-              ),
-              // OneProductWidget(
-              //   storeIndex: 14,
-              // ),
-              // OneProductWidget(
-              //   storeIndex: 15,
-              // ),
-              // OneProductWidget(
-              //   storeIndex: 16,
-              // ),
-              // OneProductWidget(
-              //   storeIndex: 17,
-              // ),
-              // OneProductWidget(
-              //   storeIndex: 2,
-              // ),
-              // OneProductWidget(
-              //   storeIndex: 1,
-              // ),
-              // OneProductWidget(
-              //   storeIndex: 3,
-              // ),
-            ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Topbar(title: "전체", showCarts: true),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text("오늘은 뭘 먹을까?", style: AppTextStyle.heading2Bold),
+              Text("지금 업데이트된 우리 동네 마감 할인 정보",
+                  style: AppTextStyle.body1Medium
+                      .copyWith(color: GlobalMainGrey.grey700)),
+              OneProductWidget(),
+            ]),
           ),
-        ),
-      ),
+          Divider(
+            color: GlobalMainGrey.grey200,
+            thickness: 2,
+          ),
+        ],
+      )),
     );
   }
 }
