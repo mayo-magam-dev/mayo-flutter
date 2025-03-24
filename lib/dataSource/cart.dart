5873d22780c60197231433fce46046ae24cf5d9b
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-03-24 20:56:31 +0900

    feat :: 엔드포인트 추가

--- lib/dataSource/cart.dart
+++ lib/dataSource/cart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mayo_flutter/model/cart/create_cart_request.dart';
+import 'package:mayo_flutter/model/cart/delete_cart_request.dart';
 import 'package:mayo_flutter/model/cart/read_cart_response.dart';
+import 'package:mayo_flutter/model/cart/update_quantity_request.dart';
 import 'package:mayo_flutter/network/dio.dart';
 
 class CartDataSource {
@@ -12,6 +14,15 @@ class CartDataSource {
     return result.map((v) => ReadCartResponse.fromJson(v)).toList();
   }
 
+  Future<void> putQuantity(String cartId, int itemQuantity) async {
+    final request =
+        UpdateQuantityRequest(cartId: cartId, itemQuantity: itemQuantity);
+    await dio.put(
+      '${dotenv.env['API_URL']}/carts',
+      data: request.toJson(),
+    );
+  }
+
   Future<void> createCart({
     required CreateCartRequest request,
   }) async {
@@ -20,4 +31,12 @@ class CartDataSource {
       data: request.toJson(),
     );
   }
-} 
+
+  Future<void> deleteCart(String cartId) async {
+    final request = DeleteCartRequest(cartId: cartId);
+    await dio.delete(
+      '${dotenv.env['API_URL']}/carts/$cartId',
+      data: request.toJson(),
+    );
+  }
+}
