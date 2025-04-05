014f107b83b8b0e0c96098598508c8f680a6c7de
Jihyuk Lee <lead@sver.dev>
2025-04-05 10:06:23 +0900

    feat :: 할인페이지 기능 구현

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -154,7 +154,7 @@ final router = GoRouter(
         ),
         GoRoute(
           path: '/on-discount',
-          builder: (context, state) => const OnDiscountPage(),
+          builder: (context, state) => OnDiscountPage(),
         ),
         GoRoute(
           path: '/orders',
