caa1a97f80f96cf347e82160de33002c1eca9996
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-04-07 20:09:44 +0900

    chore :: 불필요한 import삭제

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,3 @@
-import 'package:dio/dio.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
