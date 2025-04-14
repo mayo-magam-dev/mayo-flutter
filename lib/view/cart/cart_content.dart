a039833e6cd2d89add2fcbe849620eebd301abae
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-04-14 19:54:05 +0900

    chore :: debugPrint문 삭제

--- lib/view/cart/cart_content.dart
+++ lib/view/cart/cart_content.dart
@@ -235,7 +235,6 @@ class _CartContentState extends State<_CartContent> {
                       children: [
                         GestureDetector(
                             onTap: () async{
-                                debugPrint('2 = ${widget.cartData[1]}');
                                 await CartDataSource()
                                     .deleteCart(widget.cartData[1].cartId);
                                 context.pushReplacement('/cart');
