7679d8b48e9752b431a5252cac9db4d57d78a8d7
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2025-04-21 14:05:52 +0900

    style :: '일상에서 마요가 필요한 순간' 라우팅 적용

--- lib/view/home/home_list.dart
+++ lib/view/home/home_list.dart
@@ -1,44 +1,39 @@
 part of 'home_page.dart';
 
 class _List extends StatelessWidget {
-  //ignore: unused_element
-  const _List({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 424.h,
-      // padding: EdgeInsets.fromLTRB(24, 29, 24, 21),
       padding: EdgeInsets.fromLTRB(24, 19, 21, 0),
       child: Column(
         children: [
           Padding(
             padding: EdgeInsets.only(left: 6.w),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '일상에서 마요가 필요한 순간!',
-                  style: AppTextStyle.heading2Bold.copyWith(
-                    color: GlobalMainColor.globalPrimaryBlackColor,
-                    letterSpacing: -0.84,
+            child: GestureDetector(
+              onTap: () => context.push('/onsale'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '일상에서 마요가 필요한 순간!',
+                    style: AppTextStyle.heading2Bold.copyWith(
+                      color: GlobalMainColor.globalPrimaryBlackColor,
+                      letterSpacing: -0.84,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    context.push('/onsale');
-                  },
-                  icon: SvgPicture.asset('assets/icons/right_arrow2.svg'),
-                ),
-              ],
+                  SvgPicture.asset('assets/icons/right_arrow2.svg'),
+                ],
+              ),
             ),
           ),
           SizedBox(height: 17.h),
           BlocBuilder<HomeBloc, HomeState>(
             builder: (context, state) {
               if (state.isLoading) {
-                return const Center(child: CircularProgressIndicator());
+                return const CircularProgressIndicator(
+                    color: GlobalMainColor.globalMainColor);
               }
 
               return Padding(
