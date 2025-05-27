7cc2b13c1fc34fe7d750645fbd14f8cb9bb6e9cc
gomminjae <gommj0611@naver.com>
2025-05-28 00:16:23 +0900

    Fix: layout

--- lib/view/signUp/step1/sign_up_scaffold.dart
+++ lib/view/signUp/step1/sign_up_scaffold.dart
@@ -18,35 +18,33 @@ class _Scaffold extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: SafeArea(
-        child: Column(
-          children: [
-            topBar,
-            const Divider(height: 1, color: GlobalMainGrey.grey200),
-            Expanded(
-              child: SingleChildScrollView(
-                padding: EdgeInsets.symmetric(horizontal: 24.w),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 50.h),
-                    header,
-                    SizedBox(height: 32.h),
-                    middle,
-                    SizedBox(height: 40.h),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                24.w,
-                0,
-                24.w,
-                MediaQuery.of(context).padding.bottom + 24.h,
-              ),
-              child: nextButton,
-            ),
-          ],
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            left: 24.w,
+            right: 24.w,
+            bottom: MediaQuery.of(context).padding.bottom + 100.h, // ✅ 핵심
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              topBar,
+              const Divider(height: 1, color: GlobalMainGrey.grey200),
+              SizedBox(height: 50.h),
+              header,
+              SizedBox(height: 32.h),
+              middle,
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        minimum: EdgeInsets.fromLTRB(24.w, 0, 24.w, 57.h), // 좌우 마진 & 아래 패딩
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0), // 버튼 위쪽 간격 추가
+          child: SizedBox(
+            height: 46.h, // 원하는 버튼 높이 (혹은 Button 내부에 고정 높이)
+            child: nextButton,
+          ),
         ),
       ),
     );
